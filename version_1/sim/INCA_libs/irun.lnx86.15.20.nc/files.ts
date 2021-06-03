c47c6ac2b1977f94b1df66c437ba49772abd2098
strong-ting <s8527502987@gmail.com>
2021-06-03 21:15:50 +0800

    -update gitignore

--- version_1/sim/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++
@@ -1,3 +0,0 @@
-1173075089 /usr/cad/CBDK_IC_Contest_v2.1/Verilog/tsmc13_neg.v
-1614760955 /home/DICS_LAB/M10912039/CIC/CIC_2020_grad/version_1/sim/tb_term.sv
-1614760955 /home/DICS_LAB/M10912039/CIC/CIC_2020_grad/version_1/src/SME.v
--- version_2/sim/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++
@@ -1,4 +0,0 @@
-1173075089 /usr/cad/CBDK_IC_Contest_v2.1/Verilog/tsmc13_neg.v
-1614961693 /home/DICS_LAB/M10912039/CIC/CIC_2020_grad/version_2/src/SME.v
-1614932777 /home/DICS_LAB/M10912039/CIC/CIC_2020_grad/version_2/sim/tb_term.sv
-1614961970 /home/DICS_LAB/M10912039/CIC/CIC_2020_grad/version_2/syn/SME_syn.v
